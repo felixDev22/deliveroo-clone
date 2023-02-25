786915e5dd62dedc822bdeb7cf6455e09ee23d0b
felixDev22 <felixnzomo@gmail.com>
2023-02-25 23:20:19 +0300

    Create global styling file for the app

---
+++ styles/global.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+  },
+  profilePic: {
+    height: 50,
+    width: 50,
+    borderRadius: 50 / 2,
+  },
+});
