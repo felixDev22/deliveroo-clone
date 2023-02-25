cd4481932bc72efcf9790fe4d32ae44c0bd8c03b
felixDev22 <felixnzomo@gmail.com>
2023-02-26 02:43:02 +0300

    Add input styling

--- styles/Home.js
+++ styles/Home.js
@@ -2,12 +2,17 @@ import { StyleSheet } from 'react-native';
 
 export const homestyles = StyleSheet.create({
   home: {
-    marginVertical: 5,
+    marginVertical: 30,
     display: 'flex',
     alignItems: 'center',
     flexDirection: 'row',
     gap: 5,
+    backgroundColor: '#fff',
   },
+  rightSide: {
+    flex: 1,
+  },
+
   font1: {
     color: '#606470',
     fontSize: 13,
@@ -23,5 +28,22 @@ export const homestyles = StyleSheet.create({
   user: {
     color: '#00C2B3',
     fontSize: 25,
+  },
+  inputSection: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'row',
+    gap: 5,
+  },
+  inputBox: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 5,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: '#606470',
+    margin: 8,
+    width: 200,
   },
 });
