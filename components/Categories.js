573ada8f2327f9a3b3e8090678055f61068f003e
felixDev22 <felixnzomo@gmail.com>
2023-02-26 16:15:50 +0300

    Add category section. add images, add styling

--- components/Categories.js
+++ components/Categories.js
@@ -1,10 +1,21 @@
 import { ScrollView, Text } from 'react-native';
 import React from 'react';
+import { categoryStyles } from '../styles/category';
+import CategoryCard from './CategoryCard';
 
 const Categories = () => {
   return (
-    <ScrollView>
+    <ScrollView
+      horizontal
+      showsHorizonatalScrollIndicator={false}
+      style={categoryStyles.wrapper}>
       <Text>Categories</Text>
+
+      <CategoryCard title="food 1" />
+      <CategoryCard title="food 2" />
+      <CategoryCard title="food 3" />
+      <CategoryCard title="food 4" />
+      <CategoryCard title="food 5" />
     </ScrollView>
   );
 };
---
+++ styles/category.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const categoryStyles = StyleSheet.create({
+  wrapper: {
+    marginVertical: 20,
+  },
+  topImage: {
+    height: 70,
+    width: 70,
+    borderRadius: 20,
+    position: 'relative',
+  },
+  topText: {
+    position: 'absolute',
+    top: 5,
+    left: 5,
+  },
+});
